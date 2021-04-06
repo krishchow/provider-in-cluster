49f6b32c0ebe6649bee1b5cfac5121ba2ac73bc3
Krish Chowdhary <krish@redhat.com>
2021-04-06 17:54:34 -0400

    updated redis api

--- apis/cache/v1alpha1/redis.go
+++ apis/cache/v1alpha1/redis.go
@@ -1,12 +1,31 @@
 package v1alpha1
 
 import (
-	v1 "github.com/crossplane/crossplane-runtime/apis/common/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+
+	v1 "github.com/crossplane/crossplane-runtime/apis/common/v1"
 )
 
 // RedisParameters define the specific parameters for a Redis instance
 type RedisParameters struct {
+	// PasswordSecretRef references the secret that contains the password used
+	// in the creation of this redis instance. If no reference is given, the
+	// redis instance will be exposed with no password.
+	// +optional
+	// +immutable
+	PasswordSecretRef *v1.SecretKeySelector `json:"passwordSecretRef,omitempty"`
+
+	// ConfigVariables allows users to put arbitrary environment variables
+	// for the redis instance.
+	// +optional
+	// +immutable
+	ConfigVariables map[string]string `json:"redisConf,omitempty"`
+
+	// MemoryLimit states the maximum amount of memory available
+	// to the pod in the deployment
+	// +optional
+	// +immutable
+	MemoryLimit *string `json:"memoryLimit,omitempty"`
 }
 
 // An RedisSpec defines the desired state of a Redis instance.
